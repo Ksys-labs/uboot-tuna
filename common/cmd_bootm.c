764db4bde321d6e8a983e9c5728a2bf39ede7a1d
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2012-12-21 16:36:03 +0400

    [android]: allow force overriding boot address

--- common/cmd_bootm.c
+++ common/cmd_bootm.c
@@ -177,8 +177,20 @@ static u32 android_img_get_end(struct andr_img_hdr *hdr)
 
 static u32 android_img_get_kload(struct andr_img_hdr *hdr)
 {
+#ifdef CONFIG_ANDROID_FORCE_KERNEL_ADDR
+	return CONFIG_ANDROID_FORCE_KERNEL_ADDR;
+#endif
 	return hdr->kernel_addr;
 }
+
+static u32 android_img_get_initrd(struct andr_img_hdr *hdr)
+{
+#ifdef CONFIG_ANDROID_FORCE_INITRD_ADDR
+	return CONFIG_ANDROID_FORCE_INITRD_ADDR;
+#endif
+	return hdr->ramdisk_addr;
+}
+
 #endif
 
 bootm_headers_t images;		/* pointers to os/initrd/fdt images */
@@ -884,7 +896,8 @@ static int android_image_get_kernel(struct andr_img_hdr *hdr, int verify)
                printf("Android's image name: %s\n", andr_tmp_str);
 
        printf("Kernel load addr 0x%08x size %u KiB\n",
-                       hdr->kernel_addr, DIV_ROUND_UP(hdr->kernel_size, 1024));
+                       android_img_get_kload(hdr),
+					   DIV_ROUND_UP(hdr->kernel_size, 1024));
        strncpy(andr_tmp_str, hdr->cmdline, ANDR_BOOT_ARGS_SIZE);
        andr_tmp_str[ANDR_BOOT_ARGS_SIZE] = '\0';
        if (strlen(andr_tmp_str)) {
@@ -893,7 +906,7 @@ static int android_image_get_kernel(struct andr_img_hdr *hdr, int verify)
        }
        if (hdr->ramdisk_size)
                printf("RAM disk load addr 0x%08x size %u KiB\n",
-                               hdr->ramdisk_addr,
+                               android_img_get_initrd(hdr),
                                DIV_ROUND_UP(hdr->ramdisk_size, 1024));
        return 0;
 }
